e190784ff82baf471210a6c4fdcb7b7040b80ffb
Ashyrkina0192837465 <86921925+Ashyrkina0192837465@users.noreply.github.com>
2021-07-06 00:18:52 +0300

    АХАХАХАХХХАХА

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
-
+import random
+from random import randint
 
 if __name__ == '__main__':
     pygame.init()
@@ -10,11 +11,17 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     running = True
+    usr_x = randint(45,655)
+    usr_y = randint(45,655)
+    usr_width = 45
+    usr_height = 45
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((212, 26, 67))
+        pygame.draw.rect(screen,(247,240,22),(usr_x , usr_y , usr_width , usr_height))
+        pygame.display.update()
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
