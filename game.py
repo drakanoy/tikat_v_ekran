f9d810ed4bc76d86edb3d54a34d8f90983c972af
drakanoy <drakanoy@gmail.com>
2021-07-05 16:05:27 +0300

    добавил цикл

--- game.py
+++ game.py
@@ -1,2 +1,17 @@
 import pygame
-print('нaша игра')
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('клетчатое поле')
+    size = width, height = 700, 700
+    screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((212, 26, 67))
+        pygame.display.flip()
+    pygame.quit()
